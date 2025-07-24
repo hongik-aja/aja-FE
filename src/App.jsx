9d4bebe3d1f0f7de8170903f26131d3e71f2afca
chan000518 <wkdvnddl050701@gmail.com>
2025-07-24 12:52:11 +0900

    feat: 로그인 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from './components/layout/Layout';
 import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { KakaoRedirect } from './pages/KakaoRedirect'
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
      <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path='login' element={<Login />} />
+          <Route path="/auth/callback" element={<KakaoRedirect/>} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/KakaoRedirect.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import login from '../apis/auth';
+import {useAuthStore} from '../stores/useAuthStore'
+
+export const KakaoRedirect = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { setTokens } = useAuthStore();
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const code = searchParams.get('code');
+
+    if (!code) {
+      alert('인가 코드 없음');
+      navigate('/login');
+      return;
+    }
+
+    const kakaoLogin = async () => {
+      try {
+        // const { accessToken, refreshToken } = await login(code);
+        // setTokens(accessToken, refreshToken);
+        navigate('/');
+      } catch (err) {
+        alert('로그인 실패');
+        navigate('/login');
+      }
+    };
+
+    kakaoLogin(); 
+
+    console.log('카카오 인가 코드:', code);
+  }, [location.search, navigate, setTokens]);
+
+  return <>로딩중...</>;
+};
